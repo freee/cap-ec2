b89045d7d80942b6ea21f1840754df307b84023b
EijiSugiura <eiji.sugiura@c-fo.com>
2020-07-01 18:13:16 +0900

    Support AWS SDK v2

--- lib/cap-ec2/utils.rb
+++ lib/cap-ec2/utils.rb
@@ -1,3 +1,5 @@
+require 'aws-sdk'
+
 module CapEC2
   module Utils
 
@@ -18,6 +20,14 @@ module CapEC2
 
     def stages_tag
       fetch(:ec2_stages_tag)
+    end
+
+    def tag_delimiter
+      fetch(:ec2_tag_delimiter)
+    end
+
+    def tag_value(instance, key)
+      instance.tags.find(-> { {} }) { |t| t[:key] == key.to_s }[:value]
     end
 
     def self.contact_point_mapping
@@ -42,14 +52,23 @@ module CapEC2
     end
 
     def load_config
+      if fetch(:ec2_profile)
+        credentials = Aws::SharedCredentials.new(profile_name: fetch(:ec2_profile)).credentials
+        if credentials
+          set :ec2_access_key_id, credentials.access_key_id
+          set :ec2_secret_access_key, credentials.secret_access_key
+        end
+      end
+
       config_location = File.expand_path(fetch(:ec2_config), Dir.pwd) if fetch(:ec2_config)
       if config_location && File.exists?(config_location)
-        config = YAML.load_file fetch(:ec2_config)
+        config = YAML.load(ERB.new(File.read(fetch(:ec2_config))))
         if config
           set :ec2_host_domain, config['host_domain'] if config['host_domain']
           set :ec2_project_tag, config['project_tag'] if config['project_tag']
           set :ec2_roles_tag, config['roles_tag'] if config['roles_tag']
           set :ec2_stages_tag, config['stages_tag'] if config['stages_tag']
+          set :ec2_tag_delimiter, config['tag_delimiter'] if config['tag_delimiter']
 
           set :ec2_access_key_id, config['access_key_id'] if config['access_key_id']
           set :ec2_secret_access_key, config['secret_access_key'] if config['secret_access_key']
@@ -64,8 +83,7 @@ module CapEC2
       unless regions_array.nil? || regions_array.empty?
         return regions_array
       else
-        @ec2 = ec2_connect
-        @ec2.regions.map(&:name)
+        fail "You must specify at least one EC2 region."
       end
     end
 
